87d51da66684644cfb328feebf16d653453db97d
Antique Dernnell <a.dernnell@gmail.com>
2015-02-12 11:26:30 +0100

    worker for image preview

--- src/util/Viewer.java
+++ src/util/Viewer.java
@@ -6,14 +6,20 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryIteratorException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Path;
 import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.SwingWorker;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+
+import app.Application;
 
 public class Viewer {
 	private JLabel label;
@@ -23,22 +29,50 @@ public class Viewer {
 		label.setPreferredSize(new Dimension(300, 423));
 	}
 
-	public void setupPDF(String path) throws IOException {
-		// @TODO add workerThread
-		PDDocument document = PDDocument.load(new File(path));
-        List<PDPage> pages = document.getDocumentCatalog().getAllPages();
-        PDPage pDPage = pages.get(0);
-        BufferedImage image = pDPage.convertToImage();
-        document.close();
-        
-        setImage(image);
-        
+	public void setupPDF(final String path) throws IOException {
+
+        SwingWorker<Void, BufferedImage> worker = new SwingWorker<Void, BufferedImage>() {
+			@Override
+			protected Void doInBackground() throws Exception {
+				PDDocument document = PDDocument.load(new File(path));
+		        List<PDPage> pages = document.getDocumentCatalog().getAllPages();
+		        PDPage pDPage = pages.get(0);
+		        BufferedImage image = pDPage.convertToImage();
+		        document.close();
+		        publish(image);
+				return null;
+			}
+			
+			@Override
+			protected void process(List<BufferedImage> chunks) { // in EDT
+				setImage(chunks.get(0));
+				Application.instance().getCurrentPage().getNav().activateProgressBar(false);
+			}
+    		
+    	};
+    	Application.instance().getCurrentPage().getNav().activateProgressBar(true);
+    	worker.execute();
     }
 	
-	public void setupImage(String path) throws IOException {
-		// @TODO add workerThread
-		BufferedImage image = ImageIO.read(new File(path));
-		setImage(image);
+	public void setupImage(final String path) throws IOException {
+		
+		SwingWorker<Void, BufferedImage> worker = new SwingWorker<Void, BufferedImage>() {
+			@Override
+			protected Void doInBackground() throws Exception {
+				BufferedImage image = ImageIO.read(new File(path));
+		        publish(image);
+				return null;
+			}
+			
+			@Override
+			protected void process(List<BufferedImage> chunks) { // in EDT
+				setImage(chunks.get(0));
+				Application.instance().getCurrentPage().getNav().activateProgressBar(false);
+			}
+    		
+    	};
+    	Application.instance().getCurrentPage().getNav().activateProgressBar(true);
+    	worker.execute();
 	}
 	
 	public Container getView() {
